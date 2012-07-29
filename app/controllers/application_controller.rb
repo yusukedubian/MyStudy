b1b2346ad51e54b29eb987b444671cc5eeb97ce8
yusukedubian <svy.watanabe@gmail.com>
2012-07-29 16:27:43 +0900

    cart line_item ER図反映 ,cartのsession情報保持

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def current_cart
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
+  #上記のprivateに関して、これによりこのメソッドを使用出来るのがコントローラだけになり、されにはRailsがこのメソッドを
+  #コントローラのアクションとして公開しないようにしています。
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 # -*- coding:utf-8 -*-
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -7,4 +11,14 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)$}i,
     message: 'は、GIF,JPG,PNG画像のURLでなければいけません'
   }
+
+  private
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, '品目が存在します')
+      return false
+    end
+  end
 end
